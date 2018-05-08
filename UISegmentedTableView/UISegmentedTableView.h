7cfc307e949bed58c6b9994b4fe3cb0824403820
LinnierGames <esericksanc@gmail.com>
2018-05-07 19:24:06 -0700

    Initial Commit

---
+++ UISegmentedTableView/UISegmentedTableView.h
@@ -0,0 +1,19 @@
+//
+//  UISegmentedTableView.h
+//  UISegmentedTableView
+//
+//  Created by Erick Sanchez on 5/7/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UISegmentedTableView.
+FOUNDATION_EXPORT double UISegmentedTableViewVersionNumber;
+
+//! Project version string for UISegmentedTableView.
+FOUNDATION_EXPORT const unsigned char UISegmentedTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UISegmentedTableView/PublicHeader.h>
+
+
